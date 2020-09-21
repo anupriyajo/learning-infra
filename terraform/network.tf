784d7b0bc1298a37dfd39967ab42aab637633a50
Anupriya Johari <anupriya.johari@thoughtworks.com>
2020-09-21 22:03:56 +0200

    enable external network access

--- terraform/network.tf
+++ terraform/network.tf
@@ -34,3 +34,9 @@ resource "aws_internet_gateway" "python_gateway" {
     Name = "python-gateway"
   }
 }
+
+resource "aws_route" "python_internet_access" {
+  route_table_id = aws_vpc.python-vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.python_gateway.id
+}
