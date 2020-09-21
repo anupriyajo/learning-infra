b1cb2aad63a64a2a67674c7ea974b696324ddcfc
Anupriya Johari <anupriya.johari@thoughtworks.com>
2020-09-21 21:51:33 +0200

    add az replicated public subnet add tags

--- terraform/network.tf
+++ terraform/network.tf
@@ -12,4 +12,18 @@ resource "aws_subnet" "private" {
   cidr_block = cidrsubnet(aws_vpc.python-vpc.cidr_block, 8, count.index)
   vpc_id = aws_vpc.python-vpc.id
   availability_zone = data.aws_availability_zones.zones.names[count.index]
+  tags = {
+    Name = "python-private-subnet"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = var.az_count
+  cidr_block = cidrsubnet(aws_vpc.python-vpc.cidr_block, 8, var.az_count + count.index)
+  vpc_id = aws_vpc.python-vpc.id
+  availability_zone = data.aws_availability_zones.zones.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "python-public-subnet"
+  }
 }
