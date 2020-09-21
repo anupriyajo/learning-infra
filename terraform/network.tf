ec0ac1acd3fca724a3db720910f55994d3d31f22
Anupriya Johari <anupriya.johari@thoughtworks.com>
2020-09-21 21:37:56 +0200

    add az replicated private subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,3 +6,10 @@ resource "aws_vpc" "python-vpc" {
     Name = "python-vpc"
   }
 }
+
+resource "aws_subnet" "private" {
+  count = var.az_count
+  cidr_block = cidrsubnet(aws_vpc.python-vpc.cidr_block, 8, count.index)
+  vpc_id = aws_vpc.python-vpc.id
+  availability_zone = data.aws_availability_zones.zones.names[count.index]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "az_count" {
+  description = "number of azs to cover"
+  default = "2"
+}
